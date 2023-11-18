ce77a42457a6f1b490b562a984010bd0525d05a5
motivate-soft <minty.soft7@gmail.com>
2023-11-18 08:00:41 +0800

    Updated for new data

--- assets/js/init_autocomplete.js
+++ assets/js/init_autocomplete.js
@@ -1,165 +1,166 @@
 const initialize_autocomplete = () => {
-  const autocompleteSource = locations.map(one => {
-    return { 
-      value:one["Office (Building code)"],
+  const autocompleteSource = locations.map((one) => {
+    return {
+      value: one["Office (Building code)"],
       label: one["Country Name_revised"],
-      ...one 
-    }
+      ...one,
+    };
   });
   var searchedLongitude, searchedLatitude;
 
-   $("#view-office-search-input").autocomplete({
-        minLength: 0,
-        source: autocompleteSource,
-        position: { 
-          my: "left bottom",
-          at: "left top",
-          collision: "fit flip",
-          of: "#view-place-input-wrapper-div"
-        }, 
-        focus: function( event, ui ) {
-          $( "#view-office-search-input" ).val( ui.item.label );
-          return false;
-        },
-        select: function( event, ui ) {
-          $( "#view-office-search-input" ).val( ui.item.label );
-          $( "#autocomplete-select-id" ).val( ui.item.value );
-          searchedLongitude = ui.item["Longitude"];
-          searchedLatitude = ui.item["Latitude"];
-          map.flyTo({
-            center: [searchedLongitude, searchedLatitude],
-            essential: true, // this animation is considered essential with respect to prefers-reduced-motion
-            zoom: FLY_ZOOM
-          });
-          lastingSeconds = 0;
-          if(currentPopup) currentPopup.remove();
-          addPopup(ui.item);
-          changeDisplayData(ui.item);
-          // $( "#project-description" ).html( ui.item.desc );
-          // $( "#project-icon" ).attr( "src", "images/" + ui.item.icon );
-   
-          return false;
-        }
-      })
-      .autocomplete( "instance" )._renderItem = function( ul, item ) {
-        return $( "<li>" )
-          .append( "<div>" + item.label + "</div>" )
-          .appendTo( ul );
-      };
-    
-    map.on('zoomend', () => {
-      
+  $("#view-office-search-input")
+    .autocomplete({
+      minLength: 0,
+      source: autocompleteSource,
+      position: {
+        my: "left bottom",
+        at: "left top",
+        collision: "fit flip",
+        of: "#view-place-input-wrapper-div",
+      },
+      focus: function (event, ui) {
+        $("#view-office-search-input").val(ui.item.label);
+        return false;
+      },
+      select: function (event, ui) {
+        $("#view-office-search-input").val(ui.item.label);
+        $("#autocomplete-select-id").val(ui.item.value);
+        searchedLongitude = ui.item["Longitude"];
+        searchedLatitude = ui.item["Latitude"];
+        map.flyTo({
+          center: [searchedLongitude, searchedLatitude],
+          essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+          zoom: FLY_ZOOM,
+        });
+        lastingSeconds = 0;
+        if (currentPopup) currentPopup.remove();
+        addPopup(ui.item);
+        changeDisplayData(ui.item);
+        // $( "#project-description" ).html( ui.item.desc );
+        // $( "#project-icon" ).attr( "src", "images/" + ui.item.icon );
+
+        return false;
+      },
     })
-    
-}
+    .autocomplete("instance")._renderItem = function (ul, item) {
+    return $("<li>")
+      .append("<div>" + item.label + "</div>")
+      .appendTo(ul);
+  };
+
+  map.on("zoomend", () => {});
+};
 
 const init_air_travel_autocomplete = (data) => {
   const autocompleteData = data.map((one) => {
     return {
       label: one["route_title"],
       value: one["route_no"],
-      ...one
-    }
-  })
-  $("#view-route-search-input").autocomplete({
-    minLength: 0,
-    source: autocompleteData,
-    position: { 
-      my: "left bottom",
-      at: "left top",
-      collision: "fit flip",
-      of: "#view-place-input-wrapper-div"
-    }, 
-    // focus: function( event, ui ) {
-    //   $( "#view-route-search-input").val( ui.item["route_title"] );
-    //   return false;
-    // },
-    select: function( event, ui ) {
-      $( "#view-route-search-input" ).val( ui.item["route_title"] );
-      $( "#autocomplete-select-id" ).val( ui.item["route_no"] );
-      const lng_src = Number(ui.item["Long_src"]);
-      const lng_dst = Number(ui.item["Long_dst"]);
-      const lat_src = Number(ui.item["Lat_src"]);
-      const lat_dst = Number(ui.item["Lat_dst"]);
-      if(isNaN(lng_src) || isNaN(lng_dst) || isNaN(lat_src) || isNaN(lat_dst)) return;
-      const lng = (lng_src + lng_dst) / 2;
-      const lat = (lat_src + lat_dst) / 2;
-      console.log(ui.item);
-      console.log(lng, lat);
-      const popupPosition = new mapboxgl.LngLat(lng, lat);
-      map.flyTo({
-        center: [lng, lat],
-        essential: true, // this animation is considered essential with respect to prefers-reduced-motion
-        zoom: FLY_ZOOM
-      });
-      lastingSeconds = 0;
-      if(currentPopup) currentPopup.remove();
-      addPopup('routes', ui.item, popupPosition);
-      map.setFilter('routes-fill', [
-        '==', 'no', ui.item["route_no"]
-      ])
-      changeDisplayData(ui.item);
-      // $( "#project-description" ).html( ui.item.desc );
-      // $( "#project-icon" ).attr( "src", "images/" + ui.item.icon );
+      ...one,
+    };
+  });
+  $("#view-route-search-input")
+    .autocomplete({
+      minLength: 0,
+      source: autocompleteData,
+      position: {
+        my: "left bottom",
+        at: "left top",
+        collision: "fit flip",
+        of: "#view-place-input-wrapper-div",
+      },
+      // focus: function( event, ui ) {
+      //   $( "#view-route-search-input").val( ui.item["route_title"] );
+      //   return false;
+      // },
+      select: function (event, ui) {
+        $("#view-route-search-input").val(ui.item["route_title"]);
+        $("#autocomplete-select-id").val(ui.item["route_no"]);
+        const lng_src = Number(ui.item["Long_src"]);
+        const lng_dst = Number(ui.item["Long_dst"]);
+        const lat_src = Number(ui.item["Lat_src"]);
+        const lat_dst = Number(ui.item["Lat_dst"]);
+        if (
+          isNaN(lng_src) ||
+          isNaN(lng_dst) ||
+          isNaN(lat_src) ||
+          isNaN(lat_dst)
+        )
+          return;
+        const lng = (lng_src + lng_dst) / 2;
+        const lat = (lat_src + lat_dst) / 2;
+        console.log(ui.item);
+        console.log(lng, lat);
+        const popupPosition = new mapboxgl.LngLat(lng, lat);
+        map.flyTo({
+          center: [lng, lat],
+          essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+          zoom: FLY_ZOOM,
+        });
+        lastingSeconds = 0;
+        if (currentPopup) currentPopup.remove();
+        addPopup("routes", ui.item, popupPosition);
+        map.setFilter("routes-fill", ["==", "no", ui.item["route_no"]]);
+        changeDisplayData(ui.item);
+        // $( "#project-description" ).html( ui.item.desc );
+        // $( "#project-icon" ).attr( "src", "images/" + ui.item.icon );
 
-      return false;
-    }
-  })
-  .autocomplete( "instance" )._renderItem = function( ul, item ) {
-    return $( "<li>" )
-      .append( "<div>" + item.label + "</div>" )
-      .appendTo( ul );
+        return false;
+      },
+    })
+    .autocomplete("instance")._renderItem = function (ul, item) {
+    return $("<li>")
+      .append("<div>" + item.label + "</div>")
+      .appendTo(ul);
   };
-
-  
-  
-}
+};
 
 const init_fleet_autocomplete = (data) => {
   const autocompleteData = data.map((one) => {
     return {
-      label: one["Location"],
+      label: one["Location Name"],
       value: one["fleet_no"],
-      ...one
-    }
-  })
-  $("#view-fleet-search-input").autocomplete({
-    minLength: 0,
-    source: autocompleteData,
-    position: { 
-      my: "left bottom",
-      at: "left top",
-      collision: "fit flip",
-      of: "#view-place-input-wrapper-div"
-    }, 
-    // focus: function( event, ui ) {
-    //   $( "#view-route-search-input").val( ui.item["route_title"] );
-    //   return false;
-    // },
-    select: function( event, ui ) {
-      $( "#view-fleet-search-input" ).val( ui.item["Location"] );
-      $( "#autocomplete-select-id" ).val( ui.item["fleet_no"] );
-      const lng = Number(ui.item["Longitude"]);
-      const lat = Number(ui.item["Latitude"]);
-      const popupPosition = new mapboxgl.LngLat(lng, lat);
-      map.flyTo({
-        center: [lng, lat],
-        essential: true, // this animation is considered essential with respect to prefers-reduced-motion
-        zoom: FLY_ZOOM
-      });
-      lastingSeconds = 0;
-      if(currentPopup) currentPopup.remove();
-      addPopup('fleets', ui.item, popupPosition);
-      changeDisplayData(ui.item);
-      // $( "#project-description" ).html( ui.item.desc );
-      // $( "#project-icon" ).attr( "src", "images/" + ui.item.icon );
-
-      return false;
-      }
-    })
-    .autocomplete( "instance" )._renderItem = function( ul, item ) {
-      return $( "<li>" )
-        .append( "<div>" + item.label + "</div>" )
-        .appendTo( ul );
+      ...one,
     };
-}
+  });
+  $("#view-fleet-search-input")
+    .autocomplete({
+      minLength: 0,
+      source: autocompleteData,
+      position: {
+        my: "left bottom",
+        at: "left top",
+        collision: "fit flip",
+        of: "#view-place-input-wrapper-div",
+      },
+      // focus: function( event, ui ) {
+      //   $( "#view-route-search-input").val( ui.item["route_title"] );
+      //   return false;
+      // },
+      select: function (event, ui) {
+        $("#view-fleet-search-input").val(ui.item["Location"]);
+        $("#autocomplete-select-id").val(ui.item["fleet_no"]);
+        const lng = Number(ui.item["Longitude"]);
+        const lat = Number(ui.item["Latitude"]);
+        const popupPosition = new mapboxgl.LngLat(lng, lat);
+        map.flyTo({
+          center: [lng, lat],
+          essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+          zoom: FLY_ZOOM,
+        });
+        lastingSeconds = 0;
+        if (currentPopup) currentPopup.remove();
+        addPopup("fleets", ui.item, popupPosition);
+        changeDisplayData(ui.item);
+        // $( "#project-description" ).html( ui.item.desc );
+        // $( "#project-icon" ).attr( "src", "images/" + ui.item.icon );
+
+        return false;
+      },
+    })
+    .autocomplete("instance")._renderItem = function (ul, item) {
+    return $("<li>")
+      .append("<div>" + item.label + "</div>")
+      .appendTo(ul);
+  };
+};
--- assets/js/rotation.js
+++ assets/js/rotation.js
@@ -1,106 +1,100 @@
 // At low zooms, complete a revolution every two minutes.
-	const secondsPerRevolution = 50;
-	// Above zoom level 5, do not rotate.
-	const maxSpinZoom = 5;
-	// Rotate at intermediate speeds between zoom levels 3 and 5.
-	const slowSpinZoom = 3;
-	
-	const LASTING_SECONDS = 20;
-	let userInteracting = false;
-	let spinEnabled = true;
-	function spinGlobe() {
-		const zoom = map.getZoom();
-		if (spinEnabled && !userInteracting && zoom < maxSpinZoom) {
-			let distancePerSecond = 360 / secondsPerRevolution;
-			if (zoom > slowSpinZoom) {
-			// Slow spinning at higher zooms
-				const zoomDif =
-				(maxSpinZoom - zoom) / (maxSpinZoom - slowSpinZoom);
-				distancePerSecond *= zoomDif;
-			}
-			const center = map.getCenter();
-			center.lng -= distancePerSecond;
-			// Smoothly animate the map over one second.
-			// When this animation is complete, it calls a 'moveend' event.
-			map.easeTo({ center, duration: 1000, easing: (n) => n });
-		}
-	}
-	var lastingSeconds = 0;
-	function timeCount() {
-		if(!spinEnabled) lastingSeconds ++;
-		if(lastingSeconds == LASTING_SECONDS) {
-			if(typeof locations == "undefined") window.location.assign('index.htm');
-			spinEnabled = true;
-			if(currentPopup) currentPopup.remove();
-			map.flyTo({
-				essential: true,
-				zoom: INITIAL_ZOOM
-			})
-			if(!$("#toggle-layers-view-card").hasClass("d-none")) {
-				$("#view-toggle-layer-btn").click();
-			}
-			if(!$("#top-flights-view-card").hasClass("d-none")) $("#view-top-flights-card-btn").click();
-			$("#view-office-search-input").val("");
-			$("#ui-id-1").hide();
-			spinGlobe();
-			flyHome();
-			displayTotalValues();
-			if(typeof classes != 'undefined') {
-				init_chart(classes[classes.length - 1]);
-			}
-			lastingSeconds = 0;
-		}
-	}
+const secondsPerRevolution = 50;
+// Above zoom level 5, do not rotate.
+const maxSpinZoom = 5;
+// Rotate at intermediate speeds between zoom levels 3 and 5.
+const slowSpinZoom = 3;
 
-	function init_rotation () {
-		// Pause spinning on interaction
-		setInterval(timeCount, 1000);
-		map.on('mousedown', () => {
-			lastingSeconds = 0;
-			userInteracting = true;
-			spinEnabled = false;
-		});
-		 
-		// Restart spinning the globe when interaction is complete
-		map.on('mouseup', () => {
-			userInteracting = false;
-			
-		});
-		 
-		// These events account for cases where the mouse has moved
-		// off the map, so 'mouseup' will not be fired.
-		map.on('dragend', () => {
-			userInteracting = false;
-			lastingSeconds = 0;
-			// spinGlobe();
-		});
-		map.on('pitchend', () => {
-			userInteracting = false;
-			// spinGlobe();
-		});
-		map.on('rotateend', () => {
-			userInteracting = false;
-			// spinGlobe();
-		});
-		 
-		// When animation is complete, start spinning if there is no ongoing interaction
-		map.on('moveend', () => {
-			spinGlobe();
-		});
-		
-		document.getElementById('btn-spin').addEventListener('click', (e) => {
-			spinEnabled = !spinEnabled;
-			if (spinEnabled) {
-				spinGlobe();
-				$(e.target).removeClass("bi-play").addClass('bi-pause');
-			} else {
-				map.stop(); // Immediately end ongoing animation
-				$(e.target).removeClass("bi-pause").addClass('bi-play');
-			}
-		});
-	} 
-		
-		
-	 
-	
-	 
+const LASTING_SECONDS = 45;
+let userInteracting = false;
+let spinEnabled = true;
+function spinGlobe() {
+  const zoom = map.getZoom();
+  if (spinEnabled && !userInteracting && zoom < maxSpinZoom) {
+    let distancePerSecond = 360 / secondsPerRevolution;
+    if (zoom > slowSpinZoom) {
+      // Slow spinning at higher zooms
+      const zoomDif = (maxSpinZoom - zoom) / (maxSpinZoom - slowSpinZoom);
+      distancePerSecond *= zoomDif;
+    }
+    const center = map.getCenter();
+    center.lng -= distancePerSecond;
+    // Smoothly animate the map over one second.
+    // When this animation is complete, it calls a 'moveend' event.
+    map.easeTo({ center, duration: 1000, easing: (n) => n });
+  }
+}
+var lastingSeconds = 0;
+function timeCount() {
+  if (!spinEnabled) lastingSeconds++;
+  if (lastingSeconds == LASTING_SECONDS) {
+    if (typeof locations == "undefined") window.location.assign("index.htm");
+    spinEnabled = true;
+    if (currentPopup) currentPopup.remove();
+    map.flyTo({
+      essential: true,
+      zoom: INITIAL_ZOOM,
+    });
+    if (!$("#toggle-layers-view-card").hasClass("d-none")) {
+      $("#view-toggle-layer-btn").click();
+    }
+    if (!$("#top-flights-view-card").hasClass("d-none"))
+      $("#view-top-flights-card-btn").click();
+    $("#view-office-search-input").val("");
+    $("#ui-id-1").hide();
+    spinGlobe();
+    flyHome();
+    displayTotalValues();
+    if (typeof classes != "undefined") {
+      init_chart(classes[classes.length - 1]);
+    }
+    lastingSeconds = 0;
+  }
+}
+
+function init_rotation() {
+  // Pause spinning on interaction
+  setInterval(timeCount, 1000);
+  map.on("mousedown", () => {
+    lastingSeconds = 0;
+    userInteracting = true;
+    spinEnabled = false;
+  });
+
+  // Restart spinning the globe when interaction is complete
+  map.on("mouseup", () => {
+    userInteracting = false;
+  });
+
+  // These events account for cases where the mouse has moved
+  // off the map, so 'mouseup' will not be fired.
+  map.on("dragend", () => {
+    userInteracting = false;
+    lastingSeconds = 0;
+    // spinGlobe();
+  });
+  map.on("pitchend", () => {
+    userInteracting = false;
+    // spinGlobe();
+  });
+  map.on("rotateend", () => {
+    userInteracting = false;
+    // spinGlobe();
+  });
+
+  // When animation is complete, start spinning if there is no ongoing interaction
+  map.on("moveend", () => {
+    spinGlobe();
+  });
+
+  document.getElementById("btn-spin").addEventListener("click", (e) => {
+    spinEnabled = !spinEnabled;
+    if (spinEnabled) {
+      spinGlobe();
+      $(e.target).removeClass("bi-play").addClass("bi-pause");
+    } else {
+      map.stop(); // Immediately end ongoing animation
+      $(e.target).removeClass("bi-pause").addClass("bi-play");
+    }
+  });
+}
